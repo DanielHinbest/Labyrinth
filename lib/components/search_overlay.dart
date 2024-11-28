9f7ecfead5200def3bf7bd5a892cec84e8ec9dcd
curraje <rajecsingh0@gmail.com>
2024-11-28 00:53:25 -0500

    add tap to close search overlay

--- lib/components/search_overlay.dart
+++ lib/components/search_overlay.dart
@@ -3,7 +3,7 @@ import 'package:labyrinth/components/gui_common.dart';
 import 'package:labyrinth/game/level.dart';
 import 'package:labyrinth/screens/screen_game.dart';
 
-/// A reusable SearchOverlay widget.
+/// Search overlay for searching levels by name, difficulty, or author
 class SearchOverlay extends StatefulWidget {
   final List<Level> levels; // All levels to search through
   final void Function() onClose; // Callback for closing the overlay
@@ -34,77 +34,87 @@ class _SearchOverlayState extends State<SearchOverlay> {
         final authorMatch = (level.author).toLowerCase().contains(_searchQuery);
         return nameMatch || difficultyMatch || authorMatch;
       }).toList();
+      if (query.isEmpty) {
+        _searchResults = [];
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Positioned.fill(
-      child: Material(
-        color: Colors.black.withOpacity(0.6), // Semi-transparent background
-        child: Column(
-          children: [
-            // Search Bar
-            Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      autofocus: true,
-                      onChanged: _searchLevels,
-                      decoration: InputDecoration(
-                        hintText:
-                            "Search levels by name, difficulty, or author...",
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search),
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.clear),
-                          onPressed: widget.onClose, // Trigger close callback
+      child: GestureDetector(
+        onTap: widget.onClose,
+        child: Material(
+          color: Colors.black.withOpacity(0.6), // Semi-transparent background
+          child: Column(
+            children: [
+              // Search Bar
+              Container(
+                color: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        autofocus: true,
+                        onChanged: _searchLevels,
+                        decoration: InputDecoration(
+                          hintText:
+                              "Search levels by name, difficulty, or author...",
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search),
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.clear),
+                            onPressed: widget.onClose, // Trigger close callback
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
 
-            // Search Results
-            Expanded(
-              child: _searchQuery.isNotEmpty && _searchResults.isEmpty
-                  ? Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Text(
-                        "No results found for '$_searchQuery'",
+              // Search Results
+              Expanded(
+                child: _searchQuery.isNotEmpty && _searchResults.isEmpty ||
+                        _searchQuery.isEmpty
+                    ? Center(
+                        child: Text(
+                        _searchQuery.isEmpty
+                            ? "Type something to search..."
+                            : "No results found for '$_searchQuery'",
                         style: TextStyle(color: Colors.white70),
                       ))
-                  : ListView.builder(
-                      itemCount: _searchResults.length,
-                      itemBuilder: (context, index) {
-                        final level = _searchResults[index];
-                        // TODO: Change to use level tile
-                        return ListTile(
-                          title: Text(level.name,
-                              style: TextStyle(color: Colors.white)),
-                          subtitle: Text(
-                            "Difficulty: ${getDifficultyLabel(level.difficulty)}, Author: ${level.author}",
-                            style: TextStyle(color: Colors.white70),
-                          ),
-                          onTap: () {
-                            // Navigate to ScreenGame with the selected level
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ScreenGame(level: level),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-            ),
-          ],
+                    : ListView.builder(
+                        itemCount: _searchResults.length,
+                        itemBuilder: (context, index) {
+                          final level = _searchResults[index];
+                          // TODO: Change to use level tile
+                          return ListTile(
+                            title: Text(level.name,
+                                style: TextStyle(color: Colors.white)),
+                            subtitle: Text(
+                              "Difficulty: ${getDifficultyLabel(level.difficulty)}, Author: ${level.author}",
+                              style: TextStyle(color: Colors.white70),
+                            ),
+                            onTap: () {
+                              // Navigate to ScreenGame with the selected level
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      ScreenGame(level: level),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
     );
