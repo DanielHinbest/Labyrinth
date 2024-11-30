7643e889c9e4ccf2565e29804aaff7d8fd038210
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2024-11-30 17:45:34 -0500

    The best theme (Daniel theme) is completed.

--- lib/util/theme/daniel_theme.dart
+++ lib/util/theme/daniel_theme.dart
@@ -6,30 +6,26 @@ class DanielColors implements ThemeColors {
   static final instance = DanielColors();
 
   @override
-  Color get particleColor => Colors.purple;
+  Color get particleColor => Colors.amber;
   @override
-  Color get decorationColor => Colors.green;
+  Color get decorationColor => Colors.lightGreenAccent;
   @override
-  List<Color> get gradientColors => [
-        Colors.purple,
-        Colors.green,
-        Colors.blue,
-      ];
+  List<Color> get gradientColors => [Colors.yellow, Colors.pink, Colors.lime, Colors.cyan, Colors.orange];
 
   @override
   ThemeData get theme => ThemeData.light().copyWith(
         pageTransitionsTheme: androidTransitionTheme,
-        cardColor: Colors.purple,
-        dialogBackgroundColor: Colors.green,
-        disabledColor: Colors.blue,
-        dividerColor: Colors.purple,
-        focusColor: Colors.green,
-        hintColor: Colors.blue,
-        primaryColor: Colors.purple,
-        shadowColor: Colors.green,
-        unselectedWidgetColor: Colors.blue,
+        cardColor: Colors.yellow,
+        dialogBackgroundColor: Colors.pink,
+        disabledColor: Colors.lime,
+        dividerColor: Colors.orange,
+        focusColor: Colors.cyan,
+        hintColor: Colors.purple,
+        primaryColor: Colors.lime,
+        shadowColor: Colors.yellowAccent,
+        unselectedWidgetColor: Colors.redAccent,
         textTheme: Typography.blackCupertino.copyWith(
-          titleLarge: TextStyle(color: Colors.purple),
+          titleLarge: TextStyle(color: Colors.cyan),
         ),
       );
 }
